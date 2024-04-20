8c776a4a3279062c1927e87b486ea58daf805e4c
Nguyễn Xuân Thao <121486519+shoxie202@users.noreply.github.com>
2024-04-20 19:46:57 +0700

    Update admin.php

--- Admin/MVC/Views/login/admin.php
+++ Admin/MVC/Views/login/admin.php
@@ -120,7 +120,7 @@
          <div class="card-body">
            <div class="row no-gutters align-items-center">
              <div class="col mr-2">
-             <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Đồ khô</div>
+             <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Phụ kiện sinh nhật</div>
                <div class="h5 mb-0 font-weight-bold text-gray-800"><?=$data_tksp3['Count']?></div>
              </div>
              <div class="col-auto">
